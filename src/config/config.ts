1731eee9c64ecbf869aab3da2d4a2c185f9113e3
NvD9090 <ngovanduc.jvb@gmail.com>
2024-11-22 17:10:20 +0700

    Update 2

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 // src/config/config.ts
 const WEATHER_API_KEY = 'f5ac4be4a19c47d8a3e42522222112';
-const WEATHER_API_URL = 'http://api.weatherapi.com/v1/forecast.json';
+const WEATHER_API_URL = 'https://api.weatherapi.com/v1/forecast.json';
 
 export const api = {
   apiKey: WEATHER_API_KEY,
